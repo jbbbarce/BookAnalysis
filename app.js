94d9442765e67d8887c86135cc3bcbd85ccb6b60
John Barce <dranrebnhoj22@gmail.com>
2025-12-02 13:11:39 +0800

    Hero Sectiion and Rotating Logos

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const menuLinks = document.querySelector(".navbar__menu");
 menu.addEventListener("click", function() {
     menu.classList.toggle("is-active");
     menuLinks.classList.toggle("active");
+    document.body.classList.toggle("menu-open");
 });
+
+// Function to make Titles move
+const logos = document.querySelector(".rotating__logos-track").cloneNode(true);
+document.querySelector(".rotating__logos").appendChild(logos);
